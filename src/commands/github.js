b6f60735cab0982a89ae70b6c7b6cf780c94589c
Unknown <kacper.potyrala@gmail.com>
2018-07-08 16:24:02 +0200

    Set color

--- src/commands/github.js
+++ src/commands/github.js
@@ -11,7 +11,7 @@ try {
       "icon_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
     },
     description: turndownService.turndown('<a href="https://github.com/animek66/coin_bot">CoinBot repository link</a>'),
-    color:  0,
+    color:  2369838,
     footer: {
       icon_url: 'https://cdn.discordapp.com/avatars/395240399750299658/1e9edd0c9edf5a6edb9fd36fcd693a9f.png',
       text: 'Coinbot by Kiritito & Mickson'
